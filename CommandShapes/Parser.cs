c68e64ddae5c25253c3dbe7d8ee36c91ab98cbf0
BenIlias <benbrahimilias@gmail.com>
2024-02-27 18:27:06 +0500

    Add: Variables support
    Now the command can use variables to store values.

--- CommandShapes/Parser.cs
+++ CommandShapes/Parser.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
-using System;
+using System.Linq;
 
 namespace CommandShapes
 {
@@ -23,6 +24,9 @@ namespace CommandShapes
 		/// Check if fill is on or off
 		/// </summary>
 		public bool IsFill;
+
+		// The varibles used to store the integer values in the program.
+		public static List<Tuple<string, int>> Variables { get; private set; } = new List<Tuple<string, int>>();
 
 		/// <summary>
 		/// Color of the pen
@@ -97,24 +101,26 @@ namespace CommandShapes
 		/// <returns></returns>
 		public Color GetPenColor()
 		{
-			// default color is black
-			Color color = Color.Black;
-			if (Parameters.Count > 2)
+			try
 			{
-				switch (Parameters[2].ToLower())
-				{
-					case "red":
-						color = Color.Red;
-						break;
-					case "green":
-						color = Color.Green;
-						break;
-					case "yellow":
-						color = Color.Yellow;
-						break;
-				}
+				int ColorIndex = 0;
+				ColorIndex = Parameters.IndexOf("red");
+				if (ColorIndex > -1)
+					return Color.Red;
+
+				ColorIndex = Parameters.IndexOf("green");
+				if (ColorIndex > -1)
+					return Color.Green;
+
+				ColorIndex = Parameters.IndexOf("yellow");
+				if (ColorIndex > -1)
+					return Color.Yellow;
 			}
-			return color;
+			catch (Exception)
+			{
+			}
+			// default color is black
+			return Color.Black;
 		}
 
 		/// <summary>
@@ -153,10 +159,21 @@ namespace CommandShapes
 		{
 			try
 			{
+				List<Tuple<string, int>> variables = new List<Tuple<string, int>>();
 				// Split the input string on basis of new line
 				string[] commands = input.ToLower().Split('\n');
 				foreach (string command in commands)
 				{
+					if(IsKeyword(command))
+					{
+						continue;
+					}
+
+					if (ExecuteVariableCommand(command, variables))
+					{
+						continue;
+					}
+
 					// Split the command on basis of space
 					string[] parts = command.Split(' ');
 					int numParts = parts.Length;
@@ -165,7 +182,7 @@ namespace CommandShapes
 					if (numParts == 2 || numParts == 4 || numParts == 6)
 					{
 						// Check if the command parts are valid
-						if (IsValidCommand(parts))
+						if (IsValidCommand(parts, variables))
 						{
 							// Check if the pen color is valid
 							if (numParts > 2 && !IsValidPenColor(parts[3]))
@@ -193,13 +210,22 @@ namespace CommandShapes
 			return true;
 		}
 
+		private static bool IsKeyword(string command)
+		{
+			command = command.ToLower().Trim();
+			// Check if the color is black, red, green or yellow
+			return command == "endwhile" || command == "endif" || command.StartsWith("while") || command.StartsWith("if");
+		}
+
 		/// <summary>
 		/// Check if the command parts are valid
 		/// </summary>
 		/// <param name="parts"></param>
 		/// <returns></returns>
-		private static bool IsValidCommand(string[] parts)
+		private static bool IsValidCommand(string[] parts, List<Tuple<string, int>> variables = null)
 		{
+			if (variables == null) variables = Variables;
+
 			// Split the second part of the command on basis of comma
 			string[] parameters = parts[1].Split(',');
 
@@ -208,12 +234,27 @@ namespace CommandShapes
 				return false;
 
 			// Check if the parameters are valid integers
-			if (!int.TryParse(parameters[0], out _))
+			try
+			{
+				GetVariableValue(parameters[0], variables);
+			}
+			catch (Exception)
+			{
 				return false;
+			}
 
 			// If the length of the parameters is 2 then check if the second parameter is a valid integer
-			if (parameters.Length == 2 && !int.TryParse(parameters[1], out _))
-				return false;
+			if (parameters.Length == 2)
+			{
+				try
+				{
+					GetVariableValue(parameters[1], variables);
+				}
+				catch (Exception)
+				{
+					return false;
+				}
+			}
 
 			return true;
 		}
@@ -237,6 +278,115 @@ namespace CommandShapes
 		private static bool IsValidFill(string fill)
 		{
 			return fill.ToLower() == "on" || fill.ToLower() == "off";
+		}
+
+
+		public static int GetVariableValue(string value, List<Tuple<string, int>> variables = null)
+		{
+			if (variables == null) variables = Variables;
+			if (IsVariable(value, variables))
+			{
+				return variables.First(x => x.Item1 == value).Item2;
+			}
+			else if (int.TryParse(value, out int valueInt))
+			{
+				return valueInt;
+			}
+			else
+			{
+				throw new Exception("Invalid variable name or non-integer value");
+			}
+		}
+
+		public static bool ExecuteVariableCommand(string command, List<Tuple<string, int>> variables = null)
+		{
+			if (variables == null) variables = Variables;
+			string[] commandArgs = command.Split('=');
+
+			if (commandArgs.Length != 2)
+			{
+				return false; // Invalid command format
+			}
+
+			string variableName = commandArgs[0].Trim();
+			string variableValue = commandArgs[1].Trim();
+
+			if (variables.Any(x => x.Item1.Equals(variableName)))
+			{
+				UpdateVariable(variableName, variableValue, variables);
+			}
+			else if (int.TryParse(variableValue, out int value))
+			{
+				variables.Add(new Tuple<string, int>(variableName, value));
+			}
+
+			return true;
+		}
+
+		private static void UpdateVariable(string variableName, string variableValue, List<Tuple<string, int>> variables = null)
+		{
+			if (variables == null) variables = Variables;
+			if (TryParseExpression(variableValue, out int result, variables))
+			{
+				variables[variables.FindIndex(x => x.Item1.Equals(variableName))] = new Tuple<string, int>(variableName, result);
+			}
+			else
+			{
+				throw new Exception("Invalid expression");
+			}
+		}
+
+		private static bool TryParseExpression(string expression, out int result, List<Tuple<string, int>> variables = null)
+		{
+			if (variables == null) variables = Variables;
+			char[] operators = { '+', '-', '*', '/' };
+			foreach (var op in operators)
+			{
+				if (expression.Contains(op))
+				{
+					string[] variableValueArgs = expression.Split(op);
+					if (variableValueArgs.Length == 2)
+					{
+						int value1 = GetVariableValue(variableValueArgs[0].Trim(), variables);
+						int value2 = GetVariableValue(variableValueArgs[1].Trim(), variables);
+
+						switch (op)
+						{
+							case '+':
+								result = value1 + value2;
+								return true;
+							case '-':
+								result = value1 - value2;
+								return true;
+							case '*':
+								result = value1 * value2;
+								return true;
+							case '/':
+								if (value2 == 0)
+								{
+									throw new Exception("Cannot divide by zero");
+								}
+								result = value1 / value2;
+								return true;
+						}
+					}
+				}
+			}
+
+			if (int.TryParse(expression, out int valueInt))
+			{
+				result = valueInt;
+				return true;
+			}
+
+			result = 0;
+			return false;
+		}
+
+		public static bool IsVariable(string value, List<Tuple<string, int>> variables = null)
+		{
+			if (variables == null) variables = Variables;
+			return variables.Any(x => x.Item1 == value);
 		}
 	}
 }
